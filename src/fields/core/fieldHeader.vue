16e83bef3cbff88d11686025b08dd5fdd19808e7
Vinay <vbvinay22@gmail.com>
2018-06-22 21:06:55 +0530

    new field

---
+++ src/fields/core/fieldHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          {{schema.title}}
+        </h1>
+        <h2 class="subtitle">
+          {{schema.subtitle}}
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+	import abstractField from "../abstractField";
+
+	export default {
+		mixins: [ abstractField ]
+	};
+
+</script>
+
+
+<style lang="sass">
+</style>
