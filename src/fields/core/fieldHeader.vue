ca3bf8c0c3690f8903d4bd63fe6f85b8a2147915
Vinay <vbvinay22@gmail.com>
2018-06-22 22:54:44 +0530

    Unwanted removed

--- src/fields/core/fieldHeader.vue
+++ src/fields/core/fieldHeader.vue
@@ -1,14 +1,12 @@
 <template>
   <section class="hero" :class="schema.heroColorClass">
     <div :class="schema.heroBodyClass">
-      <div class="container">
         <h1 class="title">
           {{schema.title}}
         </h1>
         <h2 class="subtitle">
           {{schema.subtitle}}
         </h2>
-      </div>
     </div>
   </section>
 </template>
