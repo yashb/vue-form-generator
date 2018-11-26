f2a31a06c090505b501ab0762624d8b41d09a49a
Yash <yash@samitsolutions.com>
2018-11-26 14:17:38 -0500

    timer input event removed

--- src/fields/core/fieldTimer.vue
+++ src/fields/core/fieldTimer.vue
@@ -5,7 +5,38 @@
               span.settimer(style='font-size:20px;') {{ minutes }}:{{ seconds }}:{{ milliSeconds }}
               a.button.is-info.is-outlined(@click='startTimer', :disabled='isRunning') START
               a.button.is-danger.is-outlined(@click='stopTimer', :disabled='!isRunning') STOP
-              input.form-control.input(:id='getFieldID(schema)', :type='schema.inputType.toLowerCase()', :value='value', @input='onInput', @blur='onBlur', :class='schema.fieldClasses', @change='schema.onChange || null', :disabled='disabled', :accept='schema.accept', :alt='schema.alt', :autocomplete='schema.autocomplete', :checked='schema.checked', :dirname='schema.dirname', :formaction='schema.formaction', :formenctype='schema.formenctype', :formmethod='schema.formmethod', :formnovalidate='schema.formnovalidate', :formtarget='schema.formtarget', :height='schema.height', :list='schema.list', :max='schema.max', :maxlength='schema.maxlength', :min='schema.min', :minlength='schema.minlength', :multiple='schema.multiple', :name='schema.inputName', :pattern='schema.pattern', :placeholder='schema.placeholder', :readonly='schema.readonly', :required='schema.required', :size='schema.size', :src='schema.src', :step='schema.step', :width='schema.width', :files='schema.files')
+              input.form-control.input(:id='getFieldID(schema)',
+              :type='schema.inputType.toLowerCase()', 
+              :value='value',
+              :class='schema.fieldClasses',
+              :disabled='disabled',
+              :accept='schema.accept',
+              :alt='schema.alt',
+              :autocomplete='schema.autocomplete',
+              :checked='schema.checked',
+              :dirname='schema.dirname',
+              :formaction='schema.formaction',
+              :formenctype='schema.formenctype',
+              :formmethod='schema.formmethod',
+              :formnovalidate='schema.formnovalidate',
+              :formtarget='schema.formtarget',
+              :height='schema.height',
+              :list='schema.list',
+              :max='schema.max',
+              :maxlength='schema.maxlength',
+              :min='schema.min',
+              :minlength='schema.minlength',
+              :multiple='schema.multiple',
+              :name='schema.inputName',
+              :pattern='schema.pattern',
+              :placeholder='schema.placeholder',
+              :readonly='schema.readonly',
+              :required='schema.required',
+              :size='schema.size',
+              :src='schema.src',
+              :step='schema.step',
+              :width='schema.width',
+              :files='schema.files')
               span.helper(v-if="schema.inputType.toLowerCase() === 'color' || schema.inputType.toLowerCase() === 'range'") {{ value }}
 
 </template>
